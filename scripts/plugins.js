063aef150d86eb7d5f38d1cd0a2c26c5dd66acee
李伟 <style.liwei@gmail.com>
2018-10-27 11:14:48 +0800

    Update plugins.js

--- scripts/plugins.js
+++ scripts/plugins.js
@@ -5,8 +5,8 @@ hexo.extend.helper.register('theme_version', () => version)
 const source = (path, cache, ext) => {
     if (cache) {
         const minFile = `${path}${ext === '.js' ? '.min' : ''}${ext}`
-        return hexo.theme.config.cdn ? `//cdn.idrmfly.com/${name}${minFile}` : `${minFile}?v=${version}`
-        //return hexo.theme.config.cdn ? `//unpkg.com/${name}@latest${minFile}` : `${minFile}?v=${version}`
+        //return hexo.theme.config.cdn ? `//cdn.idrmfly.com/${name}${minFile}` : `${minFile}?v=${version}`
+        return hexo.theme.config.cdn ? `//unpkg.com/${name}@latest${minFile}` : `${minFile}?v=${version}`
     } else {
         return path + ext
     }
